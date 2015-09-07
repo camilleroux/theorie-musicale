bc12907699e00179a4724f345cd9227a7fcbf801
Camille Roux <roux.camille@gmail.com>
2015-09-07 23:35:09 +0200

    add interval names

--- app/models/tone.rb
+++ app/models/tone.rb
@@ -12,4 +12,12 @@ class Tone
   def as_json(options = {})
     {:tone => tone, :letter_index => letter_index, :position => position}
   end
+
+  def to_interval
+    degree = letter_index + 1
+    Interval.available_qualities_for_degree(degree).each do |quality|
+      i = Interval.new(degree, quality)
+      return i if i.semitones == tone
+    end
+  end
 end
